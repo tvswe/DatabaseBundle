1cedce7c97afb07f1ef62f1dbf0508b0a5ff4ada
Tino Vogel <tv.swe@gmx.de>
2018-03-18 10:58:36 +0100

    Connect to configured database

--- Service/DatabaseConnection.php
+++ Service/DatabaseConnection.php
@@ -2,10 +2,27 @@
 
 namespace Tvswe\DatabaseBundle\Service;
 
+use \PDO;
+
 class DatabaseConnection
 {
+    /** @var PDO */
+    private $pdo;
+    
     public function __construct(array $config) {
-        var_dump($config);
-        exit;
+        $this->connect($config);
     }
+    
+    private function connect(array $config)
+    {
+        $dsn = 'mysql:dbname=' . $config['database'] . ';host=' . $config['host'];
+        
+        try {
+            $this->pdo = new PDO($dsn, $config['user'], $config['password']);
+        } catch (PDOException $e) {
+            echo 'Connection failed: ' . $e->getMessage();
+        }
+    }
+    
+    
 }
